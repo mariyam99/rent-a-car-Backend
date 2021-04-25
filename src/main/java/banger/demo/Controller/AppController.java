0d6014994e7d980478029f16cd7d3bd956bbb0bb
Mariyam <mariyamcute684@live.com>
2021-04-25 12:17:27 +0530

    added login function

--- src/main/java/banger/demo/Controller/AppController.java
+++ src/main/java/banger/demo/Controller/AppController.java
@@ -1,10 +1,17 @@
 package banger.demo.Controller;
 
 import banger.demo.Entity.User;
+import banger.demo.JwtUtil;
 import banger.demo.Repo.UserRepo;
+import banger.demo.service.UserDetail;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -13,35 +20,56 @@ import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 @CrossOrigin(origins = "*",allowedHeaders = "*",methods = {RequestMethod.POST,RequestMethod.GET,RequestMethod.DELETE,RequestMethod.PUT})
 public class AppController {
 
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private UserDetail userDetailService;
-//
-//    @Autowired
-//    private JwtUtil jwtTokenUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetail userDetailService;
+
+    @Autowired
+    private JwtUtil jwtTokenUtil;
 
     @Autowired
     UserRepo userRepo;
 
 
+//    @PostMapping(value = "/authenticate")
+//    public ResponseEntity<?> Login(@RequestBody LoginRequest authenticationRequest) {
+//      User customer = new User(authenticationRequest.getUsername(),authenticationRequest.getPassword());
+//        User user= userRepo.findByUsername(customer.getUsername());
+//        if (user.validatePassword(authenticationRequest.getPassword(), user.getPassword())) {
+//            return new ResponseEntity<>(user,HttpStatus.OK);
+//        }
+//        return new ResponseEntity<>("Please Login again",HttpStatus.OK);
+//    }
 
-    @PostMapping(value = "/authenticate")
-    public ResponseEntity<?> Login(@RequestBody LoginRequest authenticationRequest) {
-      User customer = new User(authenticationRequest.getUsername(),authenticationRequest.getPassword());
-        User user= userRepo.findByUsername(customer.getUsername());
-        if (user.validatePassword(authenticationRequest.getPassword(), user.getPassword())) {
-            return new ResponseEntity<>(user,HttpStatus.OK);
+
+      @PostMapping(value = "/authenticate")
+      public ResponseEntity<?> createAuthenticationToken(@RequestBody SignUpRequest authenticationRequest) throws Exception {
+        User user=userRepo.findByUsername(authenticationRequest.getUsername());
+        if(user.validatePassword(authenticationRequest.getPassword(),user.getPassword())){
+            try{
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(),
+                        user.getPassword()));
+            }
+            catch (BadCredentialsException exception)
+            {
+                throw  new Exception("Incorrect username and password",exception);
+            }
         }
-        return new ResponseEntity<>("Please Login again",HttpStatus.OK);
+        final UserDetails userDetail= userDetailService
+                .loadUserByUsername(authenticationRequest.getUsername());
+        final String jwt= jwtTokenUtil.generateToken(userDetail);
+        return ResponseEntity.ok(new LoginResponse(jwt));
+      }
 
-    }
 
-    @PostMapping(value = "/findusername")
+          @PostMapping(value = "/findusername")
     public ResponseEntity<String> findusername(@Valid @RequestBody SignUpRequest signUpRequest)
     {
         String usernameResponse = "NON-EXISTS";
@@ -58,15 +86,6 @@ public class AppController {
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
 
-//        if(customerRepo.existsByUsername(signUpRequest.getUsername())) {
-//            return new ResponseEntity(new APIResponse(false, "Username is already taken!"),
-//                    HttpStatus.BAD_REQUEST);
-//        }
-//        if(customerRepo.existsByEmail(signUpRequest.getEmail())) {
-//            return new ResponseEntity(new APIResponse(false, "Email Address already in use!"),
-//                    HttpStatus.BAD_REQUEST);
-//        }
-
       User user =new User(signUpRequest.getUsername(),signUpRequest.getEmail(),new BCryptPasswordEncoder().encode(signUpRequest.getPassword()),signUpRequest.getAge(),
               signUpRequest.getFirstName(),signUpRequest.getLastName(),signUpRequest.getPhone(),signUpRequest.getLicenseNo(),signUpRequest.getNic(),
               signUpRequest.getBlacklisted(),signUpRequest.getRole());
@@ -77,5 +96,15 @@ public class AppController {
         return ResponseEntity.created(location).body(new APIResponse(true, "User registered successfully"));
     }
 
+    @PostMapping(value = "/checkprofile")
+    public ResponseEntity<?> checkProfile(@Valid @RequestBody LoginRequest Request)
+    {
+        String username_response = "NOT_AVAILABLE";
+        if(userRepo.existsByUsername(Request.getUsername())){
+            User user=userRepo.findByUsername(Request.getUsername());
+            return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+        return new ResponseEntity<>(username_response,HttpStatus.OK);
+    }
 
 }
--- src/main/java/banger/demo/service/UserDetail.java
+++ src/main/java/banger/demo/service/UserDetail.java
@@ -1,32 +1,33 @@
-//package banger.demo.service;
-//
-//import banger.demo.Entity.Customer;
-//import banger.demo.Repo.CustomerRepo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//
-//@Service
-//public class UserDetail implements UserDetailsService {
-//
-//    @Autowired
-//    private CustomerRepo customerRepo;
-//
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Customer user = customerRepo.findByUsername(username);
-//
-//        if (user == null) {
-//            throw new UsernameNotFoundException("User not found with username: " + username);
-//        }
-//        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
-//                new ArrayList<>());
-//    }
-//
-//
-//}
+package banger.demo.service;
+
+
+import banger.demo.Entity.User;
+import banger.demo.Repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class UserDetail implements UserDetailsService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                new ArrayList<>());
+    }
+
+
+}
